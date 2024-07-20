5f3a925523fae40abfe7c57486b5984b0132948a
Ømer <cph-oo203@cphbusiness.dk>
2024-07-20 16:52:07 +0200

    added AlertBox, ConfirmBox, CurrencyConverter classes

--- src/main/java/app/currencyconverter/CurrencyConverter.java
+++ src/main/java/app/currencyconverter/CurrencyConverter.java
@@ -1,2 +1,29 @@
-package app.currencyconverter;public class CurrencyConverter {
+package app.currencyconverter;
+
+public class CurrencyConverter {
+
+    public static double USDtoEUR(double USD){
+        return USD * 0.92;
+    }
+
+    public static double EURtoUSD(double EUR){
+        return EUR * 1.09;
+    }
+
+    public static double EURtoGBP(double EUR){
+        return EUR * 0.84;
+    }
+
+    public static double GBPtoEUR(double GBP){
+        return GBP * 1.19;
+    }
+
+    public static double GBPtoUSD(double GBP){
+        return GBP * 1.3;
+    }
+
+    public static double USDtoGBP(double USD){
+        return USD * 0.77;
+    }
+
 }
--- src/main/java/app/currencyconverter/Main.java
+++ src/main/java/app/currencyconverter/Main.java
@@ -1,23 +1,105 @@
 package app.currencyconverter;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+public class Main extends Application {
+
+    Stage window;
+    Scene scene;
+    Button convertButton;
+    Button exitButton;
+    TextField amountToConvert;
+    TextField convertedAmount;
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        window = primaryStage;
+        window.setTitle("Currency Converter");
+
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
+        });
+
+        Label labelFromCurrency = new Label("From currency:");
+        Label labelToCurrency = new Label("To currency:");
+
+        ChoiceBox<String> convertFrom = new ChoiceBox<>();
+        convertFrom.getItems().addAll("USD", "EUR", "GBP");
+
+        ChoiceBox<String> convertTo = new ChoiceBox<>();
+        convertTo.getItems().addAll("USD", "EUR", "GBP");
+
+        Map<String, Function<Double, Double>> conversionMap = new HashMap<>();
+        conversionMap.put("USD_EUR", CurrencyConverter::USDtoEUR);
+        conversionMap.put("EUR_USD", CurrencyConverter::EURtoUSD);
+        conversionMap.put("EUR_GBP", CurrencyConverter::EURtoGBP);
+        conversionMap.put("GBP_EUR", CurrencyConverter::GBPtoEUR);
+        conversionMap.put("GBP_USD", CurrencyConverter::GBPtoUSD);
+        conversionMap.put("USD_GBP", CurrencyConverter::USDtoGBP);
+
+        convertButton = new Button();
+        convertButton.setText("Convert");
+        convertButton.setOnAction(e -> {
+
+            String selectedFromCurrency = convertFrom.getSelectionModel().getSelectedItem();
+            String selectedToCurrency = convertTo.getSelectionModel().getSelectedItem();
+            try {
+                double amount = Double.parseDouble(amountToConvert.getText());
+
+                String key = selectedFromCurrency + "_" + selectedToCurrency;
+
+                if (conversionMap.containsKey(key)) {
+                    double converted = conversionMap.get(key).apply(amount);
+                    convertedAmount.setText(String.valueOf(converted));
+                } else {
+                    convertedAmount.setText(String.valueOf(amount));
+                }
+            } catch (NumberFormatException ex) {
+                convertedAmount.setText("Conversion not supported");
+            }
+        });
+
+        exitButton = new Button();
+        exitButton.setText("Exit");
+        exitButton.setOnAction(e -> closeProgram());
+
+        amountToConvert = new TextField();
+        convertedAmount = new TextField();
+
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20, 20, 20, 20));
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(labelFromCurrency, convertFrom, amountToConvert, labelToCurrency, convertTo, convertedAmount, convertButton, exitButton);
+
+        scene = new Scene(layout, 450, 350);
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void closeProgram() {
+        boolean answer = ConfirmBox.display("Title", "Are you sure you want to exit?");
+        if (answer) {
+            window.close();
+        }
     }
 }
